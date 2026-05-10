6d8a8f4e69ceb8b9c078495b3a43979b43920120
bhavesh <bhaveshdaswani93@gmail.com>
2026-05-10 22:10:19 +0530

    Refactor expenses management: create Expenses widget, implement ExpensesList and ExpenseItem components, and add category icons with date formatting

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,59 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    // TODO: implement createState
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _expensesData = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 15.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "Pizza",
+      amount: 9.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Text("Expense Chart"),
+          Expanded(
+            child: ExpensesList(expenses: _expensesData),
+          ),
+        ],
+      ),
+    );
+  }
+}

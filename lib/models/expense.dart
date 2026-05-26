36700af04c273e1dd560adcedd165e77873301cc
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2026-05-26 19:48:09 +0530

    Add charting functionality: implement Chart and ChartBar widgets to visualize expenses by category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -38,3 +38,25 @@ class Expense {
     return formatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  ExpenseBucket(this.category, this.expenses);
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
